97a83a405c6f84f34889b4ad1d0322d206d68434
Camille Reis <114815855+CamilleSilva@users.noreply.github.com>
2026-04-07 00:00:45 -0300

    Adicionando MatricularAluno
    CS - Matricular Aluno

---
+++ matricularAluno/matricularAluno/Program.cs
@@ -0,0 +1,127 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Cryptography.X509Certificates;
+
+namespace matricularAluno;
+
+public class Materia
+{
+    public string nomeMateria { get; set; }
+
+    public Materia(string nomeMateria)
+    {
+        this.nomeMateria = nomeMateria;
+
+    }
+
+}
+
+public class Aluno
+{
+    public string CPF { get; set; }
+
+    public List<Materia> materiasMatriculadas { get; set; } = new List<Materia>();
+
+    public Aluno(string cpf)
+    {
+        this.CPF = cpf;
+       
+
+    } 
+
+
+    public void Matricular(Materia materia)
+    {
+        materiasMatriculadas.Add(materia);
+        Console.WriteLine($"Aluno com CPF {CPF} matriculado na matéria {materia.nomeMateria}");
+        
+
+    }
+
+    public void Mostrarmaterias()
+    {
+        
+
+            foreach (var materia in materiasMatriculadas)
+            {
+
+                Console.WriteLine("\n- " + materia.nomeMateria);
+            }
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        
+
+                Console.Write("Digite o CPF do aluno: ");
+                string cpf = Console.ReadLine();
+
+
+                Aluno aluno = new Aluno(cpf);
+
+
+
+                int opcao;
+
+
+                do
+                {
+                    Console.WriteLine("\n--- MENU ---");
+                    Console.WriteLine("1 - Matricular em matéria");
+                    Console.WriteLine("2 - Ver matérias matriculadas");
+                    Console.WriteLine("0 - Sair");
+                    Console.Write("Escolha uma opção: ");
+
+                    opcao = int.Parse(Console.ReadLine());
+
+                    switch (opcao)
+                    {
+                        case 1:
+                            Console.Write("Digite o nome da matéria: ");
+                            string nomeMateria = Console.ReadLine();
+
+                            Materia materia = new Materia(nomeMateria);
+                            aluno.Matricular(materia);
+                            break;
+
+                        case 2:
+
+                            {
+                                Console.WriteLine("\nMatérias matriculadas:");
+                                aluno.Mostrarmaterias();
+                                break;
+                            }
+
+
+                        case 0:
+                            Console.WriteLine("Encerrado programa");
+                            break;
+
+                    }
+
+
+
+
+                } while (opcao != 0);
+
+
+
+            }
+
+
+
+        }
